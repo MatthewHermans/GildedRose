c1f0a55df2a050ca45d467bdd7223417e70178fa
matthew <matthew.hermans@icloud.com>
2022-01-26 13:26:57 +0100

    Flattend the if's

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -14,41 +14,17 @@ public class GildedRose {
     public func updateQuality() {
         for item in items {
             let decayRate = item.name.contains(GildedRose.ConjuredItem) ? -2 : -1 // feature proof for when multiple diffrent conjured items are added
-            if item.name != GildedRose.agedBrie && item.name != GildedRose.backstagePasses {
-                if item.name != GildedRose.sulfuras {
-                    updateQualityitem(item: item, adjustment: decayRate)
-                }
-            } else {
-                updateQualityitem(item: item, adjustment: 1)
-                
-                if item.name == GildedRose.backstagePasses {
-                    if item.sellIn < 11 {
-                        updateQualityitem(item: item, adjustment: 1)
-                    }
-                    if item.sellIn < 6 {
-                        updateQualityitem(item: item, adjustment: 1)
-                    }
-                }
-            }
+            let doesDecay = item.name != GildedRose.agedBrie && item.name != GildedRose.backstagePasses && item.name != GildedRose.sulfuras
             
-            if item.name != GildedRose.sulfuras {
-                item.sellIn -= 1
-            }
+            if doesDecay { updateQualityitem(item: item, adjustment: decayRate) }
+            if item.name == GildedRose.agedBrie { updateQualityitem(item: item, adjustment: 1) }
             
-            if item.sellIn < 0 {
-                if item.name != GildedRose.agedBrie {
-                    if item.name != GildedRose.backstagePasses {
-                        if item.name != GildedRose.sulfuras {
-                            updateQualityitem(item: item, adjustment: decayRate)
-                        }
-                        
-                    } else {
-                        item.quality -= item.quality
-                    }
-                } else {
-                    updateQualityitem(item: item, adjustment: 1)
-                }
-            }
+            let qualityIncrease = item.sellIn < 6 ? 3 : item.sellIn < 11 ? 2 : 1
+            if item.name == GildedRose.backstagePasses { updateQualityitem(item: item, adjustment: qualityIncrease) }
+            
+            if item.name != GildedRose.sulfuras { item.sellIn -= 1 }
+            
+            if item.sellIn < 0 { handleExpireditems(item: item, doesDecay: doesDecay, decayRate: decayRate) }
         }
     }
     
@@ -58,6 +34,17 @@ public class GildedRose {
         let newQuality = item.quality + adjustment
         if newQuality <= 50 && newQuality >= 0 {
             item.quality = newQuality
+        }
+    }
+    
+    fileprivate func handleExpireditems(item: Item, doesDecay: Bool, decayRate: Int) {
+        if doesDecay {
+            updateQualityitem(item: item, adjustment: decayRate)
+            // if sellin < 0 then the quality gets decreased for a second time.
+        } else if item.name != GildedRose.agedBrie && item.name == GildedRose.backstagePasses{
+            item.quality -= item.quality
+        } else {
+//                updateQualityitem(item: item, adjustment: 1)
         }
     }
 }
